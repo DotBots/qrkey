6d923fba82b78f6a683a72e25d3a413b1e00106a
Alexandre Abadie <alexandre.abadie@inria.fr>
2024-02-05 15:58:48 +0100

    js: only major/minor versions as info when deriving

--- js/src/qrkey.js
+++ js/src/qrkey.js
@@ -1,3 +1,2 @@
-export { QrkeyVersion } from "./utils/version";
 export { deriveKey, deriveTopic, encrypt, decrypt } from "./utils/crypto";
 export { useMqttBroker } from "./hooks/mqtt";
--- js/src/utils/crypto.js
+++ js/src/utils/crypto.js
@@ -1,15 +1,20 @@
 import hkdf from "futoin-hkdf";
 import * as jose from 'jose'
 
-import { QrkeyVersion } from "./version";
+import packagejson from '../../package.json';
+
+import semverMajor from 'semver/functions/major';
+import semverMinor from 'semver/functions/minor';
 
 import logger from './logger';
 const log = logger.child({module: 'crypto'});
 
+const _parsedVersion = `${semverMajor(packagejson.version)}.${semverMinor(packagejson.version)}`;
+
 export const deriveKey = (pin) => {
   const length = 32;
   const salt = '';
-  const info = `secret_key_${QrkeyVersion}`;
+  const info = `secret_key_${_parsedVersion}`;
   const hash = 'SHA-256';
 
   return hkdf(`${pin}`, length, {salt, info, hash});
@@ -18,7 +23,7 @@ export const deriveKey = (pin) => {
 export const deriveTopic = (pin) => {
   const length = 16;
   const salt = '';
-  const info = `secret_topic_${QrkeyVersion}`;
+  const info = `secret_topic_${_parsedVersion}`;
   const hash = 'SHA-256';
 
   // Topic is encoded in url safe base64
